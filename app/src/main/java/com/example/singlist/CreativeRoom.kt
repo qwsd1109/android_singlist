8bfe02e9eed04f7eee8c42aaf591388c59b75cdb
qwsd1109 <qwsd1109@gmail.com>
2019-11-06 11:01:17 +0900

    [add] creative room xml 추가

---
+++ app/src/main/java/com/example/singlist/CreativeRoom.kt
@@ -0,0 +1,12 @@
+package com.example.singlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreativeRoom : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_creative_room)
+    }
+}
--- app/src/main/java/com/example/singlist/MainActivity.kt
+++ app/src/main/java/com/example/singlist/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.singlist
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cardView.setOnClickListener{
+            val intent = Intent(this, CreativeRoom::class.java)
+            startActivity(intent)
+        }
     }
 }
